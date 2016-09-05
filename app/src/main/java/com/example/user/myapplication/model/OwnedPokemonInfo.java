4d9ff61a43380fce3542f21c0f09426a2c279188
imtomato <imtomato120@gmail.com>
2016-09-05 19:21:10 +0800

    day3_listview

---
+++ app/src/main/java/com/example/user/myapplication/model/OwnedPokemonInfo.java
@@ -0,0 +1,24 @@
+package com.example.user.myapplication.model;
+
+/**
+ * Created by user on 2016/9/5.
+ */
+public class OwnedPokemonInfo {
+
+    public static final int maxNumSkills = 4;
+    public static String[] typeNames;
+
+
+    public String name;
+    public String pokemonId;
+    public int level;
+    public int currentHP;
+    public int maxHP;
+    public int type1Index;
+    public int type2Index;
+
+    public String[] skills;
+
+    
+
+}
---
+++ app/src/main/java/com/example/user/myapplication/model/OwnedPokemonInfoDataManager.java
@@ -0,0 +1,18 @@
+package com.example.user.myapplication.model;
+
+/**
+ * Created by user on 2016/9/5.
+ */
+public class OwnedPokemonInfoDataManager {
+
+
+
+
+
+
+
+
+
+
+
+}
