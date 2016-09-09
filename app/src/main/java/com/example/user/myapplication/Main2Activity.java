a806292f0ba97c9e11d41f2dd9b80ae97a60512b
imtomato <imtomato120@gmail.com>
2016-09-09 10:08:02 +0800

    HW1_1

--- app/src/main/java/com/example/user/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/user/myapplication/Main2Activity.java
@@ -1,13 +1,26 @@
 package com.example.user.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        return false;
     }
 }
