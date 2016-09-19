74cfab2c6e4068c86240a5c13ff69692cdc63fcc
imtomato <imtomato120@gmail.com>
2016-09-19 21:59:02 +0800

    Day6_5
    Display fragment result

--- app/src/main/java/com/example/user/myapplication/TestFragmentActivity.java
+++ app/src/main/java/com/example/user/myapplication/TestFragmentActivity.java
@@ -1,13 +1,57 @@
 package com.example.user.myapplication;
 
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TestFragmentActivity extends AppCompatActivity {
+public class TestFragmentActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Fragment[] fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fragment);
+
+
+        Button f1Btn = (Button) findViewById(R.id.fragmentBtn1);
+        Button f2Btn = (Button) findViewById(R.id.fragmentBtn2);
+
+        f1Btn.setOnClickListener(this);
+        f2Btn.setOnClickListener(this);
+
+        fragment = new Fragment[2];
+        fragment[0] = TestFragment.newInstance("Fragment1");
+        fragment[1] = TestFragment.newInstance("Fragment2");
+
+    }
+
+
+    public void displayFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragmentContainer,fragment);
+        transaction.commit();
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.fragmentBtn1){
+            displayFragment(fragment[0]);
+        }else if(v.getId() == R.id.fragmentBtn2){
+            displayFragment(fragment[1]);
+        }
+
+
     }
 }
