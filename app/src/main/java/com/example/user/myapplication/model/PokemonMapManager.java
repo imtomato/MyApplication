38077494e7b6b74d8d09b25cf7315c0bbffe92ab
imtomato <imtomato120@gmail.com>
2016-09-29 22:00:10 +0800

    Day9_5
    AsyncTask split server data to get JSONArray Callback

--- app/src/main/java/com/example/user/myapplication/model/PokemonMapManager.java
+++ app/src/main/java/com/example/user/myapplication/model/PokemonMapManager.java
@@ -2,37 +2,56 @@ package com.example.user.myapplication.model;
 
 import android.os.AsyncTask;
 import android.util.Log;
+
 import com.example.user.myapplication.utility.Utils;
 import com.google.android.gms.maps.GoogleMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by user on 2016/9/29.
  */
-public class PokemonMapManager {
+public class PokemonMapManager implements RequestCallback {
 
     GoogleMap googleMap;
 
-    public PokemonMapManager(GoogleMap googleMap){
+    public PokemonMapManager(GoogleMap googleMap) {
         super();
-        this.googleMap=googleMap;
+        this.googleMap = googleMap;
     }
 
 
-    public void requestPokemonServer(){
+    public void requestPokemonServer() {
 
-        (new requestTask()).execute("http://140.112.30.42:5001/raw_data");
+        (new requestTask(this)).execute("http://140.112.30.42:5001/raw_data");
+
+    }
+
+    @Override
+    public void callback(JSONArray gyms, JSONArray pokemons, JSONArray pokeStops) {
 
     }
 
 
-    public static class requestTask extends AsyncTask<String,Void,String>{
+    public static class requestTask extends AsyncTask<String, Void, String> {
+
+        //避免memory leak,用指標暫存,減少占住記憶體
+        WeakReference<RequestCallback> requestCallbackWeakReference;
+
+        public requestTask(RequestCallback requestCallback) {
+            requestCallbackWeakReference = new WeakReference<RequestCallback>(requestCallback);
+        }
 
 
         //connect to internet
         @Override
         protected String doInBackground(String... params) {
             byte[] bytes = Utils.urlToBytes(params[0]);
-            if(bytes == null){
+            if (bytes == null) {
                 return null;
             }
             return new String(bytes);
@@ -43,14 +62,29 @@ public class PokemonMapManager {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.d("Pokemon Data:",s); //Stirng s 參數 就是 doInBackgroud做完的值
+            Log.d("Pokemon Data:", s); //Stirng s 參數 就是 doInBackgroud做完的值
+            RequestCallback requestCallback = requestCallbackWeakReference.get();
+            if(requestCallback != null && s != null){
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    JSONArray gymsArray = jsonObject.getJSONArray("gyms");
+                    JSONArray pokemonsArray = jsonObject.getJSONArray("pokemons");
+                    JSONArray pokeStopsArray = jsonObject.getJSONArray("pokestops");
+                    requestCallback.callback(gymsArray,pokemonsArray,pokeStopsArray);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
         }
-    };
 
-
-
-
-
-
-
+    }
 }
+
+interface RequestCallback {
+    void callback(JSONArray gyms,JSONArray pokemons,JSONArray pokeStops);
+}
+
+
+
